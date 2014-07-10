5623face9333f21dc7ce15d922797ac7107bd2d1
Michael Kelley <m.kelley.3154@gmail.com>
2014-07-10 13:59:06 -0500

    Hopefully fix things again.

--- app/controllers/monologue/posts_controller.rb
+++ app/controllers/monologue/posts_controller.rb
@@ -5,7 +5,7 @@ class Monologue::PostsController < Monologue::ApplicationController
   end
 
   def show
-    if main_app.current_user
+    if current_user
       @post = Monologue::Post.default.where("url = :url", {url: params[:post_url]}).first
     else
       @post = Monologue::Post.published.where("url = :url", {url: params[:post_url]}).first
